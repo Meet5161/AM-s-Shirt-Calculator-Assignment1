c3c8d5f3379d876ec4495dba47e284e0d871dd7f
Meet5161 <mchaudhari8594@conestogac.on.ca>
2021-09-30 12:34:19 +0530

    First Commit

---
+++ Meet_Chaudhari_8738594_Assignment1 Commit 1.py
@@ -0,0 +1,15 @@
+print ("The main purpose to make this program is to get familier with Python language and I really liked this project as a part of my curriculum because I learnt plenty of things while making this little bit Software. \n ")
+
+print (" Welcome to AM's Shirt Cost Calculator \n")
+
+
+print (" \n These are the available Colors we have. ")
+
+print (" 1 == Black ")
+print (" 2 == Blue ")
+print (" 3 == white ")
+print (" 4 == Yellow ")
+print (" 5 == Orange ")
+
+print (" \n What type of colour do you want to order? ")
+
